b25ba5e83121eb2993cdb2608e54118a69f0fe3f
HoangPhuc1129 <guynhp01@gmail.com>
2022-08-31 18:04:12 +0700

    fix

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -26,68 +26,41 @@ const createBtns = () => {
 createBtns();
 
 const changeColor = () => {
+  let house = document.getElementById("house");
   let btnList = document.querySelectorAll(".color-button");
-  console.log(btnList);
-  domDiv.addEventListener("click", (event) => {
-    let id = +event.target.getAttribute("data-id");
-    btnList.forEach((btn, index) => {
-      if (id == index) {
-        btnList[index].classList.add("active");
-      } else {
-        btnList[index].classList.remove("active");
+  btnList[0].classList.add("active");
+  let activeFlag = 0;
+
+  // console.log(btnList);
+  btnList.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      let id = event.target.getAttribute("data-id");
+      if (activeFlag != id) {
+        btnList[activeFlag].classList.remove("active");
       }
+      event.target.classList.add("active");
+      activeFlag = id;
+      house.setAttribute("class", `house ${colorList[activeFlag]} `);
+      // console.log(btnList);
     });
-    document
-      .getElementById("house")
-      .setAttribute(`class`, `house ${colorList[id]}`);
   });
 };
 
 changeColor();
 
-// const colorList = [
-//   "pallet",
-//   "viridian",
-//   "pewter",
-//   "cerulean",
-//   "vermillion",
-//   "lavender",
-//   "celadon",
-//   "saffron",
-//   "fuschia",
-//   "cinnabar",
-// ];
-// let domDiv = document.getElementById("colorContainer");
+// const changeColor = () => {
+//   let house = document.getElementById("house");
+//   let btnList = document.querySelectorAll(".color-button");
+//   btnList[0].classList.add("active");
+//   // console.log(btnList);
+//   btnList.forEach((btn) => {
+//     btn.addEventListener("click", (event) => {
+//       let id = event.target.getAttribute("data-id");
+//       let active = document.getElementsByClassName("active");
+//       active[0].classList.remove("active");
+//       btn.classList.add("active");
 
-// let activeFlag = -1;
-// const loadColors = () => {
-//   // create button
-//   colorList.forEach((color, index) => {
-//     let btn = document.createElement("button");
-//     domDiv.append(btn);
-//     btn.setAttribute(`class`, `color-button ${color}`);
-//     btn.setAttribute(`data-type`, `${color}`);
-//     btn.setAttribute(`data-id`, ` ${index}`);
-//   });
-
-//   // Click => change color
-//   domDiv.addEventListener("click", (event) => {
-//     event.target.classList.add("active");
-//     let elType = event.target.getAttribute(`data-type`);
-//     let id = event.target.getAttribute(`data-id`);
-//     let btnList = document.getElementsByClassName("color-button");
-
-//     for (let i = 0; i < btnList.length; i++) {
-//       if (id == i) {
-//         btnList[i].classList.add("active");
-//       } else {
-//         btnList[i].classList.remove("active");
-//       }
-//     }
-
-//     let houseColor = document.getElementById("house");
-//     houseColor.setAttribute(`class`, `house ${elType}`);
+//       house.setAttribute("class", `house ${colorList[id]} `);
+//     });
 //   });
 // };
-
-// loadColors();
